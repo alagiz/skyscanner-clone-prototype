364eae029e6ee8bfbc2822958f2e3f8c85be2715
ArtemAlagizov <alagizov@gmail.com>
2019-03-11 17:13:30 +0100

    updated generated data so that origin never equals destination

--- postgres/data/init-data.sql
+++ postgres/data/init-data.sql
@@ -17,19 +17,32 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql STRICT;
 
-CREATE OR REPLACE FUNCTION random_city()
+CREATE OR REPLACE FUNCTION random_exclusive_city(city_to_exclude VARCHAR)
   RETURNS VARCHAR AS
 $func$
 DECLARE
-  city VARCHAR [] := '{Amsterdam,Milan,London,Oslo,Vienna,Porto,Venice,Lyon,Edinburgh,Stockholm,Athens}';
+  city           VARCHAR [] := '{Amsterdam,Milan,London,Oslo,Vienna,Porto,Venice,Lyon,Edinburgh,Stockholm,Athens}';
+  city_to_return VARCHAR;
 BEGIN
-  RETURN city [random_int_between(1, 10)];
+  city_to_return := city [random_int_between(1, 10)];
+
+  WHILE city_to_return LIKE city_to_exclude LOOP
+    city_to_return := city [random_int_between(1, 10)];
+  END LOOP;
+
+  RETURN city_to_return;
 END
 $func$ LANGUAGE plpgsql VOLATILE;
 
 DO $$
+DECLARE
+  originCity      VARCHAR;
+  destinationCity VARCHAR;
 BEGIN
   FOR _ IN 1..100000 LOOP
+    originCity       := random_exclusive_city('');
+    destinationCity  := random_exclusive_city(originCity);
+
     INSERT INTO flight (
       departure,
       arrival,
@@ -39,8 +52,8 @@ BEGIN
     ) VALUES
       (now() + (random() * (INTERVAL '30 days')),
        now() + '30 days' + (random() * (INTERVAL '30 days')),
-       random_city(),
-       random_city(),
+       originCity,
+       destinationCity,
        random_int_between(1, 100));
   END LOOP;
 END; $$;
